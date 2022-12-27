8bafd59b465377de0621030b9e63dca1a28b85c3
Masaki Nakano <admin@namachan10777.dev>
2022-12-27 20:09:13 +0900

    install nix extension

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -21,13 +21,22 @@
     (with pkgs;
       vscode-with-extensions.override {
         vscodeExtensions = with vscode-extensions;
-          [ ms-vscode-remote.remote-ssh ]
-          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-            name = "remote-ssh-edit";
-            publisher = "ms-vscode-remote";
-            version = "0.47.2";
-            sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-          }];
+          [ ms-vscode-remote.remote-ssh vscodevim.vim ]
+          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+              name = "Nix";
+              publisher = "bbenoist";
+              version = "1.0.1";
+              sha256 =
+                "ab0c6a386b9b9507953f6aab2c5f789dd5ff05ef6864e9fe64c0855f5cb2a07d";
+            }
+            {
+              name = "remote-ssh-edit";
+              publisher = "ms-vscode-remote";
+              version = "0.47.2";
+              sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+            }
+          ];
       })
   ];
   services.nix-daemon.enable = true;
